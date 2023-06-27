d4847966142f74d5e1ddf73006a3986caaf2d195
Guilherme Henrique Crozariol <guicrozariol@gmail.com>
2023-06-27 16:58:32 -0300

    Create core entity + unique entity id value object

--- src/domain/entities/question.ts
+++ src/domain/entities/question.ts
@@ -1,4 +1,4 @@
-import { randomUUID } from 'node:crypto'
+import { Entity } from '@/core/entities/entity'
 import { Slug } from './value-objects/slug'
 
 interface QuestionProps {
@@ -8,18 +8,17 @@ interface QuestionProps {
   authorId: string
 }
 
-export class Question {
-  public id: string
+export class Question extends Entity {
   public title: string
   public slug: Slug
   public content: string
   public authorId: string
 
   constructor(props: QuestionProps, id?: string) {
+    super(id)
     this.title = props.title
     this.content = props.content
     this.slug = props.slug
     this.authorId = props.authorId
-    this.id = id ?? randomUUID()
   }
 }
