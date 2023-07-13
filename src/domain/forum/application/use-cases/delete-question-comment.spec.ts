bd8e72b06bd957231bf019c02f8a1b258a5dc1d7
Guilherme Henrique Crozariol <guicrozariol@gmail.com>
2023-07-13 11:17:10 -0300

    Add read notification use case + unit test + move errors to core

--- src/domain/forum/application/use-cases/delete-question-comment.spec.ts
+++ src/domain/forum/application/use-cases/delete-question-comment.spec.ts
@@ -2,8 +2,8 @@ import { expect, it, describe, beforeEach } from 'vitest'
 import { InMemoryQuestionCommentsRepository } from 'test/repositories/in-memory-question-comments-repository'
 import { DeleteQuestionCommentUseCase } from './delete-question-comment'
 import { makeQuestionComment } from 'test/factories/make-question-comment'
-import { NotAllowedError } from './errors/not-allowed-error'
-import { ResourceNotFoundError } from './errors/resource-not-found-error'
+import { NotAllowedError } from '../../../../core/errors/not-allowed-error'
+import { ResourceNotFoundError } from '@/core/errors/resource-not-found-error'
 
 let inMemoryQuestionCommentsRepository: InMemoryQuestionCommentsRepository
 let sut: DeleteQuestionCommentUseCase
